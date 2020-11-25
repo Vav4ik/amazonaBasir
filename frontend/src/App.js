b9adcfad4fd1098a5d989204d43e212b721116bc
Valdis <wawaysms@gmail.com>
2020-11-25 19:32:54 +0000

    Add React router, add screens - main and product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,61 +1,31 @@
 import React from "react";
-import data from "./data";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
-  return (
-    <div className="grid-container">
-        <header className="row">
-            <div>
-                <a className="brand" href="/">amazona</a>
-            </div>
-            <div>
-                <a href="/cart">Cart</a>
-                <a href="/signin">Sign In</a>
-            </div>
-        </header>
-        <main>
-            <div className="row center">
-                {
-                  data.products.map((product) => (
-                    <div key={product._id} className="card">
-                    <a href={`/product/${product._id}`}>
-                        <img className="medium"
-                        src={product.image}
-                        alt={product.name} />
-                    </a>
-                    <div className="card-body">
-                        <a href={`/product/${product._id}`}>
-                            <h2>{product.name}</h2>
-                        </a>
-                        <div className="rating">
-                            <span>
-                                <i className="fas fa-star"></i>
-                            </span>
-                            <span>
-                                <i className="fas fa-star"></i>
-                            </span>
-                            <span>
-                                <i className="fas fa-star"></i>
-                            </span>
-                            <span>
-                                <i className="fas fa-star"></i>
-                            </span>
-                            <span>
-                                <i className="fas fa-star-half-alt"></i>
-                            </span>
-                        </div>
-                        <div className="price">
-                            £{product.price}
-                        </div>
+    return (
+        <BrowserRouter>
+            <div className="grid-container">
+                <header className="row">
+                    <div>
+                        <a className="brand" href="/">amazona</a>
                     </div>
-                </div>
-                  ))
-                }
+                    <div>
+                        <a href="/cart">Cart</a>
+                        <a href="/signin">Sign In</a>
+                    </div>
+                </header>
+                <main>
+                    <Switch>
+                        <Route path="/" component={HomeScreen} exact />
+                        <Route path="/product/:id" component={ProductScreen} />
+                    </Switch>
+                </main>
+                <footer className="row center">2020 Copyright Waway LTD</footer>
             </div>
-        </main>
-        <footer className="row center">2020 Copyright Waway LTD</footer>
-    </div>
-  );
+        </BrowserRouter>
+    );
 }
 
 export default App;
