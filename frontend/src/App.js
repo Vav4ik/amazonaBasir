96fe546632bcdf6068f60632333b8f6fa588d2dd
Valdis <wawaysms@gmail.com>
2020-12-06 09:11:21 +0000

    Create Cart and localStorage for Cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,44 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <div className="grid-container">
-                <header className="row">
-                    <div>
-                        <a className="brand" href="/">amazona</a>
-                    </div>
-                    <div>
-                        <a href="/cart">Cart</a>
-                        <a href="/signin">Sign In</a>
-                    </div>
-                </header>
-                <main>
-                    <Switch>
-                        <Route path="/" component={HomeScreen} exact />
-                        <Route path="/product/:id" component={ProductScreen} />
-                    </Switch>
-                </main>
-                <footer className="row center">2020 Copyright Waway LTD</footer>
-            </div>
-        </BrowserRouter>
-    );
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              amazona
+            </Link>
+          </div>
+          <div>
+            <Link to="/cart">
+            Cart
+            {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+            )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
+          </div>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/" component={HomeScreen} exact />
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+          </Switch>
+        </main>
+        <footer className="row center">2020 Copyright Waway LTD</footer>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,12 +5,20 @@ import {
     createStore
 } from "redux";
 import thunk from "redux-thunk";
+import { cartReducer } from "./reducers/cartReducers";
 import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
 
-const initalState = {};
+const initalState = {
+    cart: {
+        cartItems: localStorage.getItem("cartItems")
+        ? JSON.parse(localStorage.getItem("cartItems"))
+        : []
+    }
+};
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    cart: cartReducer
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
